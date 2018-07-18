14db8b2b4e24c5dac932f00b7ad8b2717f713f4b
Sarah Nadi <nadi@ualberta.ca>
2018-07-18 11:30:41 -0600

    Make horizontal line bigger

--- src/main/java/cs/ualberta/cmput402/tictactoe/board/Board.java
+++ src/main/java/cs/ualberta/cmput402/tictactoe/board/Board.java
@@ -111,7 +111,7 @@ public class Board {
                 else
                     System.out.print(" | ");
             }
-            System.out.println("-----");
+            System.out.println("----------");
         }
     }
 
